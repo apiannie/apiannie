59dc7675869839f311478f652dc334b696c31071
Xin Zeng <zengxinzhy@gmail.com>
2022-08-11 23:44:28 +1000

    add chakra

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,32 @@
+import { Box, Heading, HStack, Link as ChakraLink } from '@chakra-ui/react';
+import { Link } from "@remix-run/react";
+
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Box fontFamily="system-ui, sans-serif" lineHeight={1.4}>
+      <Heading as="h1" color="blue.400">
+        Welcome to Remix with Chakra UI!
+      </Heading>
+      <HStack spacing={4}>
+        <ChakraLink
+          href="https://remix.run/docs"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Remix Docs
+        </ChakraLink>
+        <ChakraLink
+          target="_blank"
+          href="https://chakra-ui.com/docs/getting-started"
+          rel="noreferrer"
+        >
+          Chakra UI Docs
+        </ChakraLink>
+        <ChakraLink as={Link} color="red.400" to="/nothing-here">
+          Nothing here
+        </ChakraLink>
+      </HStack>
+    </Box>
   );
 }
